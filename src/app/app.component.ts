39a0a457344ead0bd988edd01ac174bb21a1d07a
Kotchamart Manyum <kotchamartm59@email.nu.ac.th>
2019-12-16 22:10:35 +0700

    Add websolarr

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { from } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+
   title = 'websolarr';
+  
+  constructor(private http:HttpClient){
+
+  }
+
+  ngOnInit(): void {
+    let data = {username: "koy", feedback: "I love you"};
+    this.http.post<any>('http://localhost:3000/api',data).subscribe(result=>{
+      alert(JSON.stringify(result));
+    });
+  }
 }
